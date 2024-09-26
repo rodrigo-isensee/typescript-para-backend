4624b615a2e3de21757288bdc86f01b7d4d09aeb
Rodrigo Isensee <rodrigofuinhsound@hotmail.com>
2024-09-26 16:39:33 -0300

    curso 2 aula 5

--- src/middleware/validadores/petRequestBody.ts
+++ src/middleware/validadores/petRequestBody.ts
@@ -8,18 +8,15 @@ import tratarErroValidacaoYup from "../../utils/trataValidacaoYup";
 
 yup.setLocale(pt);
 
-const esquemaBodyPet: yup.ObjectSchema<Omit<TipoRequestBodyPet, "adotante">> =
-  yup.object({
-    nome: yup.string().defined().required(),
-    especie: yup
-      .string()
-      .oneOf(Object.values(EnumEspecie))
-      .defined()
-      .required(),
-    porte: yup.string().oneOf(Object.values(EnumPorte)).defined().required(),
-    dataDeNascimento: yup.date().defined().required(),
-    adotado: yup.boolean().defined().required(),
-  });
+const esquemaBodyPet: yup.ObjectSchema<
+  Omit<TipoRequestBodyPet, "adotante" | "abrigo">
+> = yup.object({
+  nome: yup.string().defined().required(),
+  especie: yup.string().oneOf(Object.values(EnumEspecie)).defined().required(),
+  porte: yup.string().oneOf(Object.values(EnumPorte)).defined().required(),
+  dataDeNascimento: yup.date().defined().required(),
+  adotado: yup.boolean().defined().required(),
+});
 
 const middlewareValidadorBodyPet = async (
   req: Request,
--- src/middleware/verificaId.ts
+++ src/middleware/verificaId.ts
@@ -15,4 +15,5 @@ export const verificaIdMiddleware = (
       );
     }
   }
+  return next();
 };
